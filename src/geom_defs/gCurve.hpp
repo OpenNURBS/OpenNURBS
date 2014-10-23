ace1abeb5b7159272b93b1421a4e916cfad2773b
locutus <ryan@molecke.com>
2014-10-23 08:07:39 -0600

    implemented NURBS curve basis functions A2.1, A2.2 from pages 68, 70 of 'The NURBS Book'

--- src/geom_defs/gCurve.hpp
+++ src/geom_defs/gCurve.hpp
@@ -15,6 +15,7 @@
 #include <cstdint>
 #include <memory>
 #include <gPoint.hpp>
+#include <cmath>
 
 using namespace std;
 
@@ -24,14 +25,20 @@ private:
   // each curve owns a unique pointer to a vector of shared_ptrs to control points
   unique_ptr<vector<shared_ptr<gPoint<T>>>> controlPoints; // NURBS curve control points
   // each curve owns a unique pointer to vectors of knot and weight values (doubles or floats probably)
-  unique_ptr<vector<T>> knotVector; // NURBS curve knot vector (must be sequentially non-decreasing)
+  unique_ptr<vector<T>> knotVector;  // NURBS curve knot vector (must be sequentially non-decreasing)
   unique_ptr<vector<T>> knotWeights; // NURBS curve knot point weights (default is all weights = 1)
+  int degree;                        // degree of curve is one less than order of curve
+
+  int getKnotSpan(T);  // find the knot span index that a given parameter lies within
 
 public:
   gCurve(vector<shared_ptr<gPoint<T>>> &cPoints, vector<T> &kVector);
   gCurve(vector<shared_ptr<gPoint<T>>> &cPoints, vector<T> &kVector, vector<T> &kWeights);
   // the instantiators expect unique_ptrs to vectors,
   // they take control of the address pointer and memory, using a move operation with no copying
+
+  // to retrieve points along the curve
+  int getPoints(vector<shared_ptr<gPoint<T>>> &points);
 
   // control point accessors (deep copy for the get operation)
   int getControlPoints(vector<shared_ptr<gPoint<T>>> &copyCP);
@@ -44,6 +51,9 @@ public:
   // knot weight accessors
   int getKnotWeights(vector<T> &copyknotWeights);
   int setKnotWeights(vector<T> &newKnotWeights);
+
+  int basisFuns(T u, vector<T> &returnBasis);
+
 };
 
 // for templated classes, the function defs need to stay in .h or .hpp included files...
@@ -62,6 +72,9 @@ gCurve<T>::gCurve(vector<shared_ptr<gPoint<T>>> &cPoints, vector<T> &kVector) {
     knotVector->at(i) = it;
     i++;
   }
+  // default case, all knot weights are 1, indicated by knotWeights being a null_ptr
+  knotWeights = nullptr;
+  degree = knotVector->size() - controlPoints->size() -1; // degree of curve is one less than order of curve
 };
 
 template <typename T>
@@ -84,6 +97,7 @@ gCurve<T>::gCurve(vector<shared_ptr<gPoint<T>>> &cPoints, vector<T> &kVector, ve
     knotWeights->at(i) = it;
     i++;
   }
+  degree = knotVector->size() - controlPoints->size() -1;
 };
 // the instantiators expect unique_ptrs to vectors,
 // they take control of the address pointer and memory, using a move operation with no copying
@@ -124,3 +138,53 @@ int gCurve<T>::setKnotWeights(vector<T> &newKnotWeights) {
 
 }
 
+template <typename T>
+int gCurve<T>::getKnotSpan(T u){ // alg A2.1 from p.68 of "The NURBS Book"
+  int n = controlPoints->size()-1;
+  int low, mid, hi;
+  low = degree;
+  hi = n+1;
+  mid = floor((low+hi)/2);
+  // first, special case returning n
+  if(u == knotVector[hi]) return (n);
+  // a binary search to find the knotspan index
+  while(u < knotVector[mid] || u >= knotVector[mid+1]){
+    if(u < knotVector[mid]) {
+      hi = mid;
+    } else {
+      low = mid;
+    }
+    mid = floor((low+hi)/2);
+  }
+  return mid;
+}
+
+// sets values in a vector of (n+1) basis function values (floats or doubles usually)
+// note: (n+1) is also the number of control points
+// note: this is alg A2.2 from page 70 of "The NURBS Book"
+template <typename T>
+int gCurve<T>::basisFuns(T u, vector<T> &returnBasis){
+   int n = controlPoints->size()-1;
+   int i = getKnotSpan(u);
+   try {
+     returnBasis->clear();
+     returnBasis->resize(n+1);
+     returnBasis[0]=1;
+     unique_ptr<vector<T>> left(new vector<T>(n+1));
+     unique_ptr<vector<T>> right(new vector<T>(n+1));
+     for(int j=1; j<degree; j++){
+       left[j] = u - knotVector[i+1-j];
+       right[j] = knotVector[i+j] - u;
+       T saved = 0.0;
+       for (int r = 0; r<j; r++){
+         T temp = returnBasis[r]/(right[r+1]-left[j-r]);
+         returnBasis[r] = saved + right[r+1]*temp;
+         saved = left[j-r]*temp;
+       }
+       returnBasis[j] = saved;
+     }
+     return 1;
+   } catch (...) {
+     return 0;
+   }
+}
